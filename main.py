0212af2f6148592c853466bb5267bb2a086ef014
Kamyar <kmr.amh990@gmail.com>
2025-02-22 16:05:43 +0330

    ...............................

--- main.py
+++
@@ -1,3 +0,0 @@
-def show_full_name(first_name, last_name):
-    return f"{first_name} {last_name}"
-print(show_full_name("John", "Doe"))
---
+++ nw.py
@@ -0,0 +1,8 @@
+def show_full_name(first_name, last_name):
+    return f"{first_name} {last_name}"
+print(show_full_name("John", "Doe"))
+
+def show_age(age):
+    print(age)
+
+print(show_age(33))
